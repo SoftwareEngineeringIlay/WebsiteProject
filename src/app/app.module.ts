ddbe2aa7a5d2f59ac948cfb82d69eb6903a3b31e
Ilay_Nachshon <ilay.na958@gmail.com>
2024-10-28 18:52:51 +0200

    Shorting the App name.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { App } from './app.component';
 
 import { Home } from '../components/home/home.component';
 import { Electronics } from '../components/electronics/electronics.component';
@@ -10,7 +10,7 @@ import { Product } from '../components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    App,
     Product,
     Home,
     Electronics,
@@ -21,7 +21,7 @@ import { Product } from '../components/product/product.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [App]
 })
 
 export class AppModule { }
